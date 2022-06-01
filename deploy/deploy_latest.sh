e2e25b0cef0ab2ae48591458e3ad7b657804e1f9
ilhan <ilhan.myumyun@sap.com>
2022-06-01 14:42:55 +0300

    chore: test push

--- deploy/deploy_latest.sh
+++ deploy/deploy_latest.sh
@@ -89,14 +89,14 @@ git add .
 git commit -m "Automatic update by Github Actions CI #$TRAVIS_BUILD_NUMBER, commit ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy/deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add ../deploy_key
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy/deploy_key.enc -out ../deploy_key -d
+# chmod 600 ../deploy_key
+# eval `ssh-agent -s`
+# ssh-add ../deploy_key
 
 # Push new change
 git push $SSH_REPO gh-pages
